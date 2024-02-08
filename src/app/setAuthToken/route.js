2140ae51ff9e6b1aefaff2c19f153630a53a612d
Leo Mancini <mancini@leo.gd>
2024-02-08 16:50:33 -0500

    set cookie to expire in 1 year

--- src/app/setAuthToken/route.js
+++ src/app/setAuthToken/route.js
@@ -5,8 +5,9 @@ import { cookies } from "next/headers";
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const authToken = searchParams.get("token");
+  const oneYear = 365 * 24 * 60 * 60 * 1000;
 
-  cookies().set("authToken", authToken);
+  cookies().set("authToken", authToken, { expires: Date.now() + oneYear });
 
   return Response.json({});
 }
