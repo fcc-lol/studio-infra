57f6fb7a8a77bd76351640dcd7e916b3b2ac4dad
Leo Mancini <mancini@leo.gd>
2024-02-08 15:24:01 -0500

    updates styling

--- src/app/inventory/[slug]/page.jsx
+++ src/app/inventory/[slug]/page.jsx
@@ -2,21 +2,70 @@
 
 import React from "react";
 import useGetAirtableRecord from "@/hooks/useGetAirtableRecord";
+import styled from "styled-components";
+
+const Title = styled.div`
+  font-size: 3.5rem;
+  line-height: 4rem;
+  font-weight: bold;
+  margin-bottom: 3rem;
+  padding-right: 1rem;
+`;
+const Fields = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+const Field = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+const Label = styled.div`
+  font-size: 0.75rem;
+  line-height: 1.25rem;
+  text-transform: uppercase;
+  font-weight: 500;
+  letter-spacing: 0.25rem;
+
+  color: rgba(0, 0, 0, 0.5);
+  @media (prefers-color-scheme: dark) {
+    color: rgba(255, 255, 255, 0.5);
+  }
+`;
+const Value = styled.div`
+  font-size: 1.25rem;
+  line-height: 2rem;
+  font-weight: regular;
+
+  color: rgba(0, 0, 0, 0.8);
+  @media (prefers-color-scheme: dark) {
+    color: rgba(255, 255, 255, 0.8);
+  }
+`;
 
 const Item = ({ params }) => {
   const { record, error, loading } = useGetAirtableRecord(params.slug);
 
   return (
     <div>
-      {record &&
-        record.fields &&
-        Object.keys(record.fields).map((key) => {
-          return (
-            <div key={key}>
-              {key}: {record.fields[key]}
-            </div>
-          );
-        })}
+      {record && record.fields && (
+        <>
+          <Title>{record.fields["Name"]}</Title>
+          <Fields>
+            {record.fields["Description"] && (
+              <Field>
+                <Label>Description</Label>
+                <Value>{record.fields["Description"]}</Value>
+              </Field>
+            )}
+            <Field>
+              <Label>Owner</Label>
+              <Value>{record.fields["Owner"]}</Value>
+            </Field>
+          </Fields>
+        </>
+      )}
     </div>
   );
 };
