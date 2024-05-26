9e926a54c51a2a32a18183d7c3acfcf084b823fc
Leo Mancini <mancini@leo.gd>
2024-05-26 16:46:26 -0400

    Adding additional styling

--- src/app/components/item.jsx
+++ src/app/components/item.jsx
@@ -3,18 +3,38 @@
 import React from "react";
 import useGetAirtableRecord from "@/hooks/useGetAirtableRecord";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
+const Card = styled.div`
+  width: 100%;
+  border-radius: 1rem;
+  padding: 2rem;
+  background: white;
+  filter: drop-shadow(0px 1px 2px rgba(0, 0, 0, 0.03))
+    drop-shadow(0px 2px 6px rgba(0, 0, 0, 0.03))
+    drop-shadow(0px 2px 32px rgba(0, 0, 0, 0.1));
+  @media (prefers-color-scheme: dark) {
+    background: rgba(255, 255, 255, 0.125);
+  }
+`;
+const Icon = styled.div`
+  width: 4rem;
+  height: 4rem;
+  font-size: 3rem;
+  margin: 0.5rem 0 1rem 0;
+`;
 const Title = styled.div`
-  font-size: 3.5rem;
-  line-height: 4rem;
+  font-size: 2.5rem;
+  line-height: 3.5rem;
   font-weight: bold;
-  margin-bottom: 3rem;
+  margin-bottom: 2.5rem;
   padding-right: 1rem;
 `;
 const Fields = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
+  gap: 2rem;
 `;
 const Field = styled.div`
   display: flex;
@@ -26,17 +46,17 @@ const Label = styled.div`
   line-height: 1.25rem;
   text-transform: uppercase;
   font-weight: 500;
-  letter-spacing: 0.25rem;
+  letter-spacing: 0.125rem;
 
-  color: rgba(0, 0, 0, 0.5);
+  color: rgba(0, 0, 0, 0.25);
   @media (prefers-color-scheme: dark) {
-    color: rgba(255, 255, 255, 0.5);
+    color: rgba(255, 255, 255, 0.25);
   }
 `;
 const Value = styled.div`
-  font-size: 1.25rem;
-  line-height: 2rem;
-  font-weight: regular;
+  font-size: 1.125rem;
+  line-height: 1.625rem;
+  font-weight: 400;
 
   color: rgba(0, 0, 0, 0.8);
   @media (prefers-color-scheme: dark) {
@@ -50,13 +70,16 @@ const Item = ({ slug }) => {
   return (
     <div>
       {record && record.fields && (
-        <>
+        <Card>
+          <Icon>
+            <FontAwesomeIcon icon={fas[`fa${record.fields["Icon"]}`]} />
+          </Icon>
           <Title>{record.fields["Name"]}</Title>
           <Fields>
-            {record.fields["Description"] && (
+            {record.fields["Notes"] && (
               <Field>
-                <Label>Description</Label>
-                <Value>{record.fields["Description"]}</Value>
+                <Label>Notes</Label>
+                <Value>{record.fields["Notes"]}</Value>
               </Field>
             )}
             <Field>
@@ -64,7 +87,7 @@ const Item = ({ slug }) => {
               <Value>{record.fields["Owner"]}</Value>
             </Field>
           </Fields>
-        </>
+        </Card>
       )}
     </div>
   );
